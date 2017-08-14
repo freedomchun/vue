e8bf7326458ad89811b287f5b8b4e82a77b08bd7
bing8u <676659348@qq.com>
2017-08-14 17:17:28 +0800

    update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,8 +13,6 @@ export const getLoginUser = () => {
 export const setLoginUser = (resData) => {
     // 设置权限
     sessionStorage.setItem('permissions', JSON.stringify(resData.permissions));
-    // 设置角色
-    sessionStorage.setItem('roles', JSON.stringify(resData.roles));
     // 设置登录用户
     sessionStorage.setItem('loginUser', JSON.stringify(resData.userInfo));
 };
