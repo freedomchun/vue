2e2279f262f94ea62a67644d1522254e112d69b2
freedomchun <95354716@qq.com>
2017-08-14 15:41:38 +0800

    update

--- src/views/picture/Pic.vue
+++ src/views/picture/Pic.vue
@@ -16,57 +16,58 @@
 		</el-form>
 		<el-row>
 			<el-col :span="6">
-				<el-tree :data="data" :props="defaultProps" accordion @node-click="handleNodeClick">
-				</el-tree>
+				<el-tree :data="data" :props="defaultProps" accordion @node-click="handleNodeClick"></el-tree>
 			</el-col>
 		</el-row>
 	</div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        data: [{label: '我的图片 1',
-         children: [{
-            label: '二级 1-1',
-           
-          }]	
-        }
-        {
-          label: '我的图片 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
-    },
-    methods: {
-      handleNodeClick(data) {
-        console.log(data);
-      }
-    }
-  };
+	export default {
+		data() {
+			return {
+				data: [{
+						label: '我的图片 1',
+						children: [{
+							label: '二级 1-1',
+
+						}]
+					},
+					{
+						label: '我的图片 1',
+						children: [{
+							label: '二级 1-1',
+							children: [{
+								label: '三级 1-1-1'
+							}]
+						}]
+					}, {
+						label: '一级 2',
+						children: [{
+							label: '二级 2-1',
+							children: [{
+								label: '三级 2-1-1'
+							}]
+						}, {
+							label: '二级 2-2',
+							children: [{
+								label: '三级 2-2-1'
+							}]
+						}]
+					}
+				],
+				defaultProps: {
+					children: 'children',
+					label: 'label'
+				}
+			};
+		},
+		methods: {
+			handleNodeClick(data) {
+				console.log(data);
+			}
+		}
+	};
 </script>
 
 <style scoped>
